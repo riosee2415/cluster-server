a04f9e1ef328ed7ba1146bcb73cef702b935704b
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-01-05 05:17:36 +0900

    a

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "CLUSTER_TEST",
-      script: "babel-node ./app.js",
+      name: `CLUSTER_TEST`,
+      script: `babel-node ./app.js`,
       instances: 0,
-      exec_mode: "cluster",
+      exec_mode: `cluster`,
     },
   ],
 };

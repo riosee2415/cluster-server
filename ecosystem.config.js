9a2d17a64d6196075a76c8011584b630f5c24af4
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-01-05 05:37:14 +0900

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,6 @@ module.exports = {
       script: `pm2 start npm -- run start`,
       instances: 0,
       exec_mode: `cluster`,
-      watch: true,
-      ignore_watch: ["node_modules"],
-      interpreter: "./node_modules/.bin/babel-node",
-      source_map_support: true,
     },
   ],
 };

3e10a413a2c7f67a5a87971b5f57621dfa4dbc3d
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-01-05 05:23:53 +0900

    A

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -5,6 +5,10 @@ module.exports = {
       script: `babel-node ./app.js`,
       instances: 0,
       exec_mode: `cluster`,
+      watch: true,
+      ignore_watch: ["node_modules"],
+      interpreter: "./node_modules/.bin/babel-node",
+      source_map_support: true,
     },
   ],
 };

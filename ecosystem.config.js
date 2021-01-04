95224cbcc81e06b56cb5b0dec927980688d4fe77
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-01-05 05:54:16 +0900

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "CLUSTER_TEST",
-      script: "pm2 start -i 0 --name 'test' -- run start",
+      script: "./src/app.js",
       instances: 0,
       exec_mode: "cluster",
     },
